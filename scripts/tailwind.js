8dc88194a67f02fc1cfebc67a4f78a882afedd6a
Umraz <kumraz858@gmail.com>
2022-03-15 19:22:11 +0530

    Initial

---
+++ scripts/tailwind.js
@@ -0,0 +1,30 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          black: '#181818',
+          carbon: '#000',
+          primary: '#E01E35'
+        }
+      },
+      fontFamily: {
+          'poppins': [
+              'Poppins',
+              'sans-serif'
+          ],
+          'extenda': [
+              'Extenda',
+              'sans-serif'
+          ],
+          'feutro': [
+              'Feturo',
+              'sans-serif'
+          ],
+          'tex': [
+              'TeX',
+              'sans-serif'
+          ]
+      }
+    }
+  }
+
