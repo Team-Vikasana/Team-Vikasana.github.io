1aabafa9b0d6e8122bba8a2fed04de9ef5512e18
Umraz <kumraz858@gmail.com>
2022-03-23 00:47:50 +0530

    vue update

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
+const navbar = document.querySelector('#nav');
+window.onscroll = () => {
+    if (window.scrollY > 10) {
+        navbar.classList.add('nav-scrolled');
+    } else {
+        navbar.classList.remove('nav-scrolled');
+    }
+};
+
 const btn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
 
